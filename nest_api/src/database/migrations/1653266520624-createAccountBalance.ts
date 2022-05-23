8f8d60b7179ac417118898e4734b702470472261
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-22 22:30:46 -0300

    Create account_balance table migration

---
+++ nest_api/src/database/migrations/1653266520624-createAccountBalance.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class accountBalance1653242794061 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'account_balance',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'account_id',
+            type: 'int',
+          },
+          {
+            name: 'balance',
+            type: 'numeric',
+            isUnique: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('account_balance');
+  }
+}
