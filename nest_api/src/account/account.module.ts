c1751d14983192ea62d39c9d06f13e1e4a0c8fff
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-23 00:07:37 -0300

    Update account module imports

--- nest_api/src/account/account.module.ts
+++ nest_api/src/account/account.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
+import { AccountBalanceController } from './balance/accountBalance.controller';
+import { AccountBalance } from './balance/accountBalance.entity';
+import { AccountBalanceService } from './balance/accountBalance.service';
 import { Account } from './entities/account.entity';
 import { UtilsService } from './utils/utils.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
-  controllers: [AccountController],
-  providers: [AccountService, UtilsService],
+  imports: [TypeOrmModule.forFeature([Account, AccountBalance])],
+  controllers: [AccountController, AccountBalanceController],
+  providers: [AccountService, UtilsService, AccountBalanceService],
 })
 export class AccountModule {}
