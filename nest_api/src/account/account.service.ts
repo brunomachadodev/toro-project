e595b03e1d70834683fe1926cf9032e599ae9f97
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-24 00:35:14 -0300

    Update account service with handle account creation method

--- nest_api/src/account/account.service.ts
+++ nest_api/src/account/account.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AccountBalanceService } from './balance/accountBalance.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { Account } from './entities/account.entity';
 
@@ -9,9 +10,23 @@ export class AccountService {
   constructor(
     @InjectRepository(Account)
     private readonly accountRepository: Repository<Account>,
+    private readonly accountBalanceService: AccountBalanceService,
   ) {}
 
-  public async create(createAccountDto: CreateAccountDto): Promise<Account> {
+  public async handleAccountCreation(
+    createAccountDto: CreateAccountDto,
+  ): Promise<Account> {
+    const account = await this.create(createAccountDto);
+    if (account)
+      await this.accountBalanceService.create({
+        accountId: account.id,
+        balance: 0,
+      });
+
+    return account;
+  }
+
+  private async create(createAccountDto: CreateAccountDto): Promise<Account> {
     const account = this.accountRepository.create(createAccountDto);
     return await this.accountRepository.save(account);
   }
