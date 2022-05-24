cdebf279cb2e7bd3f7d48d9892ca99e33b41181c
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-24 00:34:39 -0300

    Update account module controllers and providers imported

--- nest_api/src/account/account.module.ts
+++ nest_api/src/account/account.module.ts
@@ -6,11 +6,18 @@ import { AccountBalanceController } from './balance/accountBalance.controller';
 import { AccountBalance } from './balance/accountBalance.entity';
 import { AccountBalanceService } from './balance/accountBalance.service';
 import { Account } from './entities/account.entity';
+import { EventsController } from './events/events.controller';
+import { EventsService } from './events/events.service';
 import { UtilsService } from './utils/utils.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account, AccountBalance])],
-  controllers: [AccountController, AccountBalanceController],
-  providers: [AccountService, UtilsService, AccountBalanceService],
+  controllers: [AccountController, AccountBalanceController, EventsController],
+  providers: [
+    AccountService,
+    UtilsService,
+    AccountBalanceService,
+    EventsService,
+  ],
 })
 export class AccountModule {}
