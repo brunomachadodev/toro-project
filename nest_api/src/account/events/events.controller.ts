402eef4a1214d18afe2164b9dd97d3620af15cd7
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-24 00:36:59 -0300

    Update events controller with method sketch

--- nest_api/src/account/events/events.controller.ts
+++ nest_api/src/account/events/events.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { DepositEventDto } from './dto/deposit-event.dto';
 
 @Controller('account/events')
-export class EventsController {}
+export class EventsController {
+  @Post('deposit')
+  public async createTransaction(@Body() depositDto: DepositEventDto) {
+    return depositDto;
+  }
+}
