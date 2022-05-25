1615bdd21c964d7a12ce009cf2da949bc1cd432a
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-25 01:23:42 -0300

    Create deposit-event dto

--- nest_api/src/account/events/dto/deposit-event.dto.ts
+++ nest_api/src/account/events/dto/deposit-event.dto.ts
@@ -6,7 +6,7 @@ import {
   IsNumberString,
   IsOptional,
   IsString,
-  ValidateNested
+  ValidateNested,
 } from 'class-validator';
 
 class Target {
@@ -48,6 +48,25 @@ export class DepositEventDto {
   public target: Target;
 
   @IsOptional()
+  @Type(() => Origin)
+  @ValidateNested()
+  public origin?: Origin;
+
+  @IsNumber()
+  public amount: number;
+}
+
+export class TransferEventDto {
+  @IsNotEmpty()
+  @IsString()
+  public event: string;
+
+  @IsNotEmptyObject()
+  @Type(() => Target)
+  @ValidateNested()
+  public target: Target;
+
+  @IsNotEmptyObject()
   @Type(() => Origin)
   @ValidateNested()
   public origin: Origin;
