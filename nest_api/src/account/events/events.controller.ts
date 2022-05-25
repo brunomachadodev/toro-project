c6959759af0533d8026ca47045f1f7b654a0e549
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-25 01:20:32 -0300

    Update events controller, handling multiples event types

--- nest_api/src/account/events/events.controller.ts
+++ nest_api/src/account/events/events.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { DepositEventDto } from './dto/deposit-event.dto';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { EventsService } from './events.service';
 
 @Controller('account/events')
 export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+  private logger: Logger = new Logger(EventsController.name);
+
   @Post('deposit')
-  public async createTransaction(@Body() depositDto: DepositEventDto) {
-    return depositDto;
+  public async createTransaction(@Body() depositDto: any) {
+    try {
+      if (depositDto.event === 'TRANSFER' || depositDto.event === 'PIX') {
+        return await this.eventsService.handleTransferTransaction(depositDto);
+      } else if (depositDto.event === 'DEPOSIT') {
+        return await this.eventsService.handleDepositTransaction(depositDto);
+      }
+    } catch (error) {
+      this.logger.error(error);
+    }
   }
 }
