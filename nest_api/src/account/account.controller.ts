862a4d49613198b272fbbeecbad0f45b567f1d56
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-25 01:30:03 -0300

    Refactor account controller

--- nest_api/src/account/account.controller.ts
+++ nest_api/src/account/account.controller.ts
@@ -20,14 +20,13 @@ export class AccountController {
     const parsedData = createAccountDto;
     parsedData.cpf = this.utilsService.formatCPF(createAccountDto.cpf);
 
-    const isEmailUsed = await this.accountService.checkIfEmailExists(
+    const accountUsingEmail = await this.accountService.findByEmail(
       parsedData.email,
     );
-    const isCpfUsed = await this.accountService.checkIfCpfExists(
-      parsedData.cpf,
-    );
 
-    if (!isEmailUsed && !isCpfUsed) {
+    const accountUsingCpf = await this.accountService.findByCpf(parsedData.cpf);
+
+    if (!accountUsingEmail && !accountUsingCpf) {
       const accountData = await this.accountService.handleAccountCreation(
         createAccountDto,
       );
@@ -35,8 +34,8 @@ export class AccountController {
     } else {
       return response.status(409).send({
         error: `Não foi possível criar uma nova conta. ${
-          isEmailUsed ? 'Email já cadastrado. ' : ''
-        }${isCpfUsed ? 'CPF já cadastrado.' : ''}`,
+          accountUsingEmail ? 'Email já cadastrado. ' : ''
+        }${accountUsingCpf ? 'CPF já cadastrado.' : ''}`,
       });
     }
   }
