268b0f7eef99bf3c8309ad257957f52ca0e44a8a
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-25 23:13:19 -0300

    Create generic app error service

---
+++ nest_api/src/utils/appError.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppErrorService {
+  public readonly message: string;
+
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
