a7cd7335edba047160ab07f7825952d383ee41d5
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-26 17:41:17 -0300

    Update accountCard style rules

--- next_client/src/components/accountCard.tsx
+++ next_client/src/components/accountCard.tsx
@@ -6,7 +6,7 @@ interface AccountCardProps {
 
 export default function AccountCard({ data }: AccountCardProps): ReactElement {
   return (
-    <section className='bg-white shadow overflow-hidden max-w-3xl sm:rounded-lg mt-4 lg:mx-60 md:mx-10 sm:mx-8 xs:mx-9 '>
+    <section className='bg-white shadow-md overflow-hidden max-w-3xl sm:rounded-lg mt-4 mx-4 md:mx-auto  '>
       <header className='px-4 py-5 sm:px-6'>
         <h3 className='text-lg leading-6 font-medium text-gray-900'>
           Informações para depósito
@@ -15,12 +15,12 @@ export default function AccountCard({ data }: AccountCardProps): ReactElement {
           Sua conta Toro aceita transferências por TED e PIX, desde que a conta possua o mesmo CPF cadastrado.
         </p>
       </header>
-      <div className='border-t border-gray-200'>
+      <div className='border-t border-gray-300'>
         <dl>
           {data.map((item) => (
             <div
               key={item.name}
-              className='odd:bg-white even:bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'
+              className='odd:bg-white even:bg-gray-50 px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'
             >
               <dt className='text-sm font-medium text-gray-500'>{item.name}</dt>
               <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
