eb6a036e44f5e9603f6a3ea923f9b57d5188d4cd
Bruno Machado <41761319+brunomachadodev@users.noreply.github.com>
2022-05-25 23:14:37 -0300

    Add CPF validation method to account utils service

--- nest_api/src/account/utils/utils.service.ts
+++ nest_api/src/account/utils/utils.service.ts
@@ -5,4 +5,26 @@ export class UtilsService {
   public formatCpf(cpf: string) {
     return cpf.replace(/[^\d]/g, '');
   }
+
+  public validateCpf(inputCpf: string) {
+    let sum = 0;
+    let remainder: number;
+
+    if (inputCpf == '00000000000') return false;
+    for (let i = 1; i <= 9; i++)
+      sum = sum + parseInt(inputCpf.substring(i - 1, i)) * (11 - i);
+    remainder = (sum * 10) % 11;
+
+    if (remainder == 10 || remainder == 11) remainder = 0;
+    if (remainder != parseInt(inputCpf.substring(9, 10))) return false;
+
+    sum = 0;
+    for (let i = 1; i <= 10; i++)
+      sum = sum + parseInt(inputCpf.substring(i - 1, i)) * (12 - i);
+    remainder = (sum * 10) % 11;
+
+    if (remainder == 10 || remainder == 11) remainder = 0;
+    if (remainder != parseInt(inputCpf.substring(10, 11))) return false;
+    return true;
+  }
 }
